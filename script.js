db20783f3383b628e29b333749a5b722125f6d19
Joao Victor Homem Almeida <121456112+JoaoAlmeida05@users.noreply.github.com>
2024-05-18 17:11:02 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+window.onscroll = function() {myFunction()};
+    
+var header = document.getElementById("header")
+    
+var sticky = header.offsetTop;
+    
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+    } else {
+    header.classList.remove("sticky");
+    }
+}
+
+
+function myFunction(){
+    document.getElementById("dropdown-content1").classList.toggle("show");
+}
+
+
+window.onclick = function(event) {
+    if (!event.target.matches('.one')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content1");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  } 
